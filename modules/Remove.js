89f40e9d64c4815b830381658ac56b36c3f6f6a3
Donice <technocrater.pdevelopers@gmail.com>
2023-08-17 15:18:57 +0100

    IMplemented modules

---
+++ modules/Remove.js
@@ -0,0 +1,14 @@
+export const removeBookFromDOM = (listBooks, id) => {
+    if (id !== -1) {
+      listBooks.removeBook(id);
+    }
+  };
+  
+  export const addRemoveButtons = (listBooks) => {
+    const buttons = document.getElementsByClassName('btn remove-btn');
+    for (let i = 0; i < buttons.length; i += 1) {
+      buttons[i].addEventListener('click', () => {
+        removeBookFromDOM(listBooks, i);
+      });
+    }
+  };
