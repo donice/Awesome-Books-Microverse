86c14db40e7821d22e42bbf452024f388b66ad25
Donice <technocrater.pdevelopers@gmail.com>
2023-08-17 15:32:14 +0100

    Fixed linter errors

--- modules/index.js
+++ modules/index.js
@@ -1,7 +1,7 @@
-import { addMenuLinks } from './Menu.js';
-import { displayTime } from './Clock.js';
-import { removeBookFromDOM, addRemoveButtons } from './Remove.js';
-import Book from './Book.js';
+import { addMenuLinks } from "./Menu.js";
+import { displayTime } from "./Clock.js";
+import { removeBookFromDOM, addRemoveButtons } from "./Remove.js";
+import Book from "./Book.js";
 
 class Library {
   constructor() {
@@ -10,12 +10,12 @@ class Library {
 
   // Local Storage
   saveToLocalStorage() {
-    localStorage.setItem('MY-Library', JSON.stringify(this.books));
+    localStorage.setItem("MY-Library", JSON.stringify(this.books));
   }
 
   getDataFromLocalStorage() {
     try {
-      const data = JSON.parse(localStorage.getItem('MY-Library'));
+      const data = JSON.parse(localStorage.getItem("MY-Library"));
       if (data !== null) {
         this.books = data;
       }
@@ -25,9 +25,9 @@ class Library {
   }
 
   addBook() {
-    const form = document.querySelector('#form');
-    const title = document.querySelector('#title');
-    const author = document.querySelector('#author');
+    const form = document.querySelector("#form");
+    const title = document.querySelector("#title");
+    const author = document.querySelector("#author");
     const bookTitle = title.value;
     const bookAuthor = author.value;
 
@@ -37,18 +37,18 @@ class Library {
       this.saveToLocalStorage();
       this.getBooks();
       form.reset();
-      document.getElementById('msg-add-book').innerHTML = 'New book added...';
+      document.getElementById("msg-add-book").innerHTML = "New book added...";
       setTimeout(() => {
-        document.getElementById('msg-add-book').innerHTML = '';
+        document.getElementById("msg-add-book").innerHTML = "";
       }, 3000);
     }
     addRemoveButtons(this);
   }
 
   getBooks() {
-    const section = document.querySelector('#book-list');
+    const section = document.querySelector("#book-list");
     this.getDataFromLocalStorage();
-    let books = '<table>';
+    let books = "<table>";
     this.books.forEach((book, index) => {
       books += `<tr>
       <td>
@@ -63,7 +63,7 @@ class Library {
     if (this.books.length === 0) {
       books += '<tr><td<p class="empty-libray">Empty...</p></td></tr>';
     }
-    books += '</table>';
+    books += "</table>";
     section.innerHTML = books;
   }
 
@@ -79,8 +79,8 @@ class Library {
 // let listBooks
 const listBooks = new Library();
 
-const form = document.querySelector('#form');
-form.addEventListener('submit', (event) => {
+const form = document.querySelector("#form");
+form.addEventListener("submit", (event) => {
   event.preventDefault();
   listBooks.addBook();
 });
