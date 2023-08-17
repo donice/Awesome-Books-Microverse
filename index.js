5504aca1bbc53f2117966a6dabf85386e5c1d943
Donice <technocrater.pdevelopers@gmail.com>
2023-08-17 15:18:31 +0100

    Removed unused files

--- index.js
+++
@@ -1,9 +0,0 @@
-import { handleFormSubmit, updateBookList } from './UI.js';
-import Library from './Library.js';
-
-const form = document.querySelector('#form');
-form.addEventListener('submit', handleFormSubmit);
-
-const listBooks = new Library();
-listBooks.getDataFromLocalStorage();
-updateBookList();
